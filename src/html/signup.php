c5361ee1d4f08c8607a74867aae335dee94a36ba
RobertErlandsson <dat12rer@student.lu.se>
2016-09-20 13:48:00 +0200

    uppdaterat login men fel vid authenticate

--- src/html/signup.php
+++ src/html/signup.php
@@ -2,16 +2,5 @@
 
  
 
-if(!empty($_POST['inputUsername']) && !empty($_POST['inputPassword'])
-	&& !empty($_POST['inputAddress']) && !empty($_POST['inputConfirmPassword'])){
-
-$USERNAME=
-$PASSWORD=
-$ADDRESS=
-
-$query = mysqli_query($con, "INSERT INTO `users` (`USERNAME`, `PASSWORD`, `ADDRESS`) VALUES
-('$username', '$password', '$address');");
-
-}
 ?>
 
